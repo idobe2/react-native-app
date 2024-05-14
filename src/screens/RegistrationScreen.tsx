f39e7e9f6852f403395d9142f039a5d619a53111
Ido <idobenoun@gmail.com>
2024-05-14 23:48:13 +0300

    Add additional user data
    LInked by object id

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, Alert, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import config from './../core/config';
 
 const RegistrationScreen: React.FC = () => {
+
+  // User input state
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [accessToken, setAccessToken] = useState('');
 
-  const handleRegistration = async () => {
+  // Student input state
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUserRegistration = async () => {
+    setIsLoading(true);
     if (email === '' || password === '') {
       Alert.alert('Please fill in all fields');
       return;
@@ -19,17 +29,12 @@ const RegistrationScreen: React.FC = () => {
     try {
       const responseFromServer = await axios.post(`${config.serverAddress}/auth/register`,
         { email, password }
-
       );
       if (responseFromServer.status === 200) {
-        console.log("Register successful");
-        alert("Register successful");
-        setEmail('');
-        setPassword('');
-      }
-      else {
-        console.log("Login failed with status: ", responseFromServer.status);
-      }
+        console.log("User: Register successful");
+        await new Promise(f => setTimeout(f, 5000));
+        await handleStudentRegistration(extractIdFromResponse(responseFromServer.request._response));
+      } else { console.log("Login failed with status: ", responseFromServer.status); }
     }
     catch (error: unknown) {
       if (axios.isAxiosError(error)) {
@@ -37,8 +42,46 @@ const RegistrationScreen: React.FC = () => {
         console.log("Login failed with error: ", error.message);
         if (error.response) {
           console.log("Error status: ", error.response.status);
-          // alert(`Login failed: ${error.response.data.message}`);
           alert("Login failed: user already exists");
+        } else { alert("Login failed: Network error or server is down");}
+      } else {
+        // Error is not an AxiosError, handle it differently
+        console.log("An unexpected error occurred:", error);
+        alert("An unexpected error occurred");
+      }
+    }
+  finally { setIsLoading(false); }
+  };
+
+  const extractIdFromResponse = (response: string ): string => {
+    try {
+      const parsedResponse = JSON.parse(response);
+      console.log("id: ", parsedResponse._id);
+      return parsedResponse._id; // Access the _id field
+    } catch (error) {
+      console.error("Failed to parse JSON", error);
+      return ""; // Return an empty string or handle the error as needed
+    }
+  };
+
+  const getAccessToken = async () => {
+    try {
+      const responseFromServer = await axios.post(
+        `${config.serverAddress}/auth/login`,
+        { email, password }
+      );
+      if (responseFromServer.status === 200) {
+        setAccessToken(responseFromServer.data.accessToken);
+      } else {
+        console.log("Login failed with status: ", responseFromServer.status);
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        // Error is an AxiosError, now you can access error.response, error.message, etc.
+        console.log("Login failed with error: ", error.message);
+        if (error.response) {
+          console.log("Error status: ", error.response.status);
+          alert(`Login failed: ${error.response.data.message}`);
         } else {
           alert("Login failed: Network error or server is down");
         }
@@ -48,11 +91,65 @@ const RegistrationScreen: React.FC = () => {
         alert("An unexpected error occurred");
       }
     }
-  };
+  }
+
+  const handleStudentRegistration = async (_id: String) => {
+    getAccessToken();
+    console.log("Access token: ", accessToken);
+    try {
+      const responseFromServer = await axios.post(`${config.serverAddress}/student`,
+        { _id, name, age },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+
+      if (responseFromServer.status === 201) {
+        console.log("Student: Register successful");
+        alert("Register successful");
+        // Clear the input fields
+        setEmail('');
+        setPassword('');
+        setName('');
+        setAge('');
+      }
+      else {
+        console.log("Login failed with status: ", responseFromServer.status);
+      }
+  }
+  catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      // Error is an AxiosError, now you can access error.response, error.message, etc.
+      console.log("Login failed with error: ", error.message);
+      if (error.response) {
+        console.log("Error status: ", error.response.status);
+        alert(`Login failed: ${error.response.data.message}`);
+      } else {
+        alert("Login failed: Network error or server is down");
+      }
+    } else {
+      // Error is not an AxiosError, handle it differently
+      console.log("An unexpected error occurred:", error);
+      alert("An unexpected error occurred");
+    }
+  }
+};
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Register</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Age"
+        value={age}
+        onChangeText={setAge}
+        autoCapitalize="none"
+      />
       <TextInput
         style={styles.input}
         placeholder="Username"
@@ -68,7 +165,17 @@ const RegistrationScreen: React.FC = () => {
         secureTextEntry={true}
         autoCapitalize="none"
       />
-      <Button title="Register" onPress={handleRegistration} />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        autoCapitalize="none"
+      />
+      {isLoading ? (
+        <ActivityIndicator size="large" /> // Display the loading indicator
+      ) : ( <Button title="Register" onPress={handleUserRegistration} /> )}
     </View>
   );
 };
